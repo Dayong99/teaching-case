a0814db5bfcd4bf0822dee6b1bea053df2350fe8
Dayong99 <2484030571@qq.com>
2020-08-30 21:14:27 +0800

    '202008302114'

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-<<<<<<< HEAD
+
   ],
   plugins: [
     [
@@ -11,7 +11,6 @@ module.exports = {
         styleLibraryName: 'theme-chalk'
       }
     ]
-=======
->>>>>>> 955ad59d90991cb0f3bd92d4277d39d6b3f83ad5
+
   ]
 }
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
     <!--    路由占位符-->
     <router-view></router-view>
   </div>
@@ -17,35 +16,5 @@ export default {
 
 <style>
 
-=======
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
-</template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
->>>>>>> 955ad59d90991cb0f3bd92d4277d39d6b3f83ad5
 </style>
+
